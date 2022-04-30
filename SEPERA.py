f21b007180cd8284806efaf2b84bf556db15c449
Jethro Kwong <62458997+JCCKwong@users.noreply.github.com>
2022-04-30 14:39:32 -0400

    Update SEPERA.py

--- SEPERA.py
+++ SEPERA.py
@@ -345,8 +345,8 @@ def full_app(session_state):
 
                 # Overlay text showing Gleason Grade Group, % positive cores, and % core involvement for each site
                 draw.text((735, 1820), base_L, fill="black", font=font, align="center")
-                draw.text((700, 690), mid_L, fill="black", font=font, align="center")
-                draw.text((525, 275), apex_L, fill="black", font=font, align="center")
+                draw.text((735, 1130), mid_L, fill="black", font=font, align="center")
+                draw.text((735, 375), apex_L, fill="black", font=font, align="center")
                 draw.text((1300, 1110), base_R, fill="black", font=font, align="center")
                 draw.text((1590, 690), mid_R, fill="black", font=font, align="center")
                 draw.text((1300, 275), apex_R, fill="black", font=font, align="center")
