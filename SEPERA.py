242e929f8d91427135ecc3137d710724f1a334e2
Jethro Kwong <62458997+JCCKwong@users.noreply.github.com>
2022-05-01 05:55:20 -0400

    Update SEPERA.py

--- SEPERA.py
+++ SEPERA.py
@@ -337,13 +337,13 @@ def full_app(session_state):
                 draw.text((1850, 1920), base_R, fill="black", font=font, align="center")
                 draw.text((1850, 1190), mid_R, fill="black", font=font, align="center")
                 draw.text((1770, 545), apex_R, fill="black", font=font, align="center")
-                st.image(image2, use_column_width='auto')
+    st.image(image2, use_column_width='auto')
 
-                left_prob = (model.predict_proba(pt_features)[:, 1]*100).round()
-                right_prob = (model.predict_proba(pt_features_r)[:, 1]*100).round()
-                st.header('Your Results')
-                st.subheader('Probability of RIGHT extraprostatic extension is {:} %.'.format(str(left_prob)[1:-2]))
-                st.subheader('Probability of LEFT extraprostatic extension is {:} %.'.format(str(right_prob)[1:-2]))
+    left_prob = (model.predict_proba(pt_features)[:, 1]*100).round()
+    right_prob = (model.predict_proba(pt_features_r)[:, 1]*100).round()
+    st.header('Your Results')
+    st.subheader('Probability of RIGHT extraprostatic extension is {:} %.'.format(str(left_prob)[1:-2]))
+    st.subheader('Probability of LEFT extraprostatic extension is {:} %.'.format(str(right_prob)[1:-2]))
 
 
 def about(session_state):
