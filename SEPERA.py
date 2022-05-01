166a3ff2a34db7c6fb6f338ee0545f008cdface1
Jethro Kwong <62458997+JCCKwong@users.noreply.github.com>
2022-05-01 05:14:48 -0400

    Update SEPERA.py

--- SEPERA.py
+++ SEPERA.py
@@ -347,8 +347,8 @@ def full_app(session_state):
 
                 left_prob = (model.predict_proba(pt_features)[:, 1]*100).round(0)
                 right_prob = (model.predict_proba(pt_features_r)[:, 1]*100).round(0)
-                col2.subheader('Probability of RIGHT extraprostatic extension is {:}%'.format(left_prob))
-                col2.subheader('Probability of LEFT extraprostatic extension is {:}%'.format(right_prob))
+                col2.subheader('Probability of RIGHT extraprostatic extension is {:}%'.format(str(left_prob)))
+                col2.subheader('Probability of LEFT extraprostatic extension is {:}%'.format(str(right_prob)))
 
 
 def about(session_state):
