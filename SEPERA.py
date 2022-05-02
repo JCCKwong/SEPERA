0028fc2bbcc2e135bcd043d42e8d1b9e4ca3ed5b
Jethro Kwong <62458997+JCCKwong@users.noreply.github.com>
2022-05-02 02:41:06 -0400

    Update SEPERA.py

--- SEPERA.py
+++ SEPERA.py
@@ -383,9 +383,9 @@ def about(session_state):
     st.write("""""")
     st.image("Images/THP.png", width=400, caption="Trillium Health Partners, Mississauga, Ontario, Canada")
     st.write("""""")
-    st.image("Images/IMM.png", width=400, caption="L'Institut Mutualiste Montsouris, Paris, France")
+    st.image("Images/IMM.png", width=300, caption="L'Institut Mutualiste Montsouris, Paris, France")
     st.write("""""")
-    st.image("Images/JB.png", width=400, caption="Jules Bordet Institute, Brussels, Belgium")
+    st.image("Images/JB.png", width=300, caption="Jules Bordet Institute, Brussels, Belgium")
 
 if __name__ == "__main__":
     st.set_page_config(page_title="SEPERA - Side-Specific Extra-Prostatic Extension Risk Assessment",
