a220dffae32b8122dc2120671548a53abc8d5970
Jethro Kwong <62458997+JCCKwong@users.noreply.github.com>
2022-05-02 02:50:37 -0400

    Update SEPERA.py

--- SEPERA.py
+++ SEPERA.py
@@ -379,7 +379,7 @@ def about(session_state):
     """
     )
     st.header("Contributing Institutions")
-    UHN, THP, IMM, JB = st.columns([1.5, 1.5, 1, 1])
+    UHN, THP, IMM, JB = st.columns([1.4, 1.4, 1, 1])
     UHN.image("Images/UHN.png", width=400, caption="University Health Network, Toronto, Ontario, Canada")
     THP.image("Images/THP.png", width=400, caption="Trillium Health Partners, Mississauga, Ontario, Canada")
     IMM.image("Images/IMM.png", width=240)
