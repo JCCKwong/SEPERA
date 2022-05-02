56bb1c7ebbf7b474f9837e95f6316edf9d30eb9e
Jethro Kwong <62458997+JCCKwong@users.noreply.github.com>
2022-05-02 01:14:57 -0400

    Update SEPERA.py

--- SEPERA.py
+++ SEPERA.py
@@ -47,7 +47,7 @@ def full_app(session_state):
     """
     )
 
-    col1 = st.columns([1])
+    col1, col2 = st.columns([3, 1])
     col1.header('Prostate Diagram')
     col1.write('Automatically updates based on individualized patient characteristics.')
 
@@ -335,7 +335,7 @@ def full_app(session_state):
                 draw.text((1850, 1920), base_R, fill="black", font=font, align="center")
                 draw.text((1850, 1190), mid_R, fill="black", font=font, align="center")
                 draw.text((1770, 545), apex_R, fill="black", font=font, align="center")
-                col1.image(image2, use_column_width='auto')
+                col1.image(image2, width=600, use_column_width='auto')
 
                 left_prob = str((model.predict_proba(pt_features)[:, 1]*100).round())[1:-2]
                 right_prob = str((model.predict_proba(pt_features_r)[:, 1]*100).round())[1:-2]
