680d53e1de4dbb349e87fac6738bb85a57c5cad9
Jethro Kwong <62458997+JCCKwong@users.noreply.github.com>
2022-05-03 02:03:51 -0400

    Update SEPERA.py

--- SEPERA.py
+++ SEPERA.py
@@ -92,8 +92,8 @@ def page_sepera():
     st.header("Enter patient information")
     if st.button('Click here if you have received hormone therapy or radiation therapy for prostate cancer before your '
                  'radical prostatectomy'):
-        st.warning('The results of SEPERA are not applicable for you since this tool was only trained on patients who '
-                 'did not receive hormone or radiation therapy prior to their surgery.')
+        st.warning('The results of SEPERA will not be applicable for you since this tool was only trained on patients '
+                   'who did not receive hormone or radiation therapy prior to their surgery.')
     col1, col2, col3 = st.columns([1, 1, 1])
 
     with st.form(key="my_form"):
